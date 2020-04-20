1c5e8de24192e1416ac3159ef234d1e180acb1a4
Luis Cuevas <luisdavid25540055@gmail.com>
2020-04-20 18:36:45 -0500

    comienzo

---
+++ precache-manifest.5d4b4fa4b62cf7801e168c72fdfe8066.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14992f555624be10596450df1f0a6e05",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a35910a300bef4c4a106",
+    "url": "/static/css/main.ad55ae80.chunk.css"
+  },
+  {
+    "revision": "083c131d71fc1bf066da",
+    "url": "/static/js/2.9438baa3.chunk.js"
+  },
+  {
+    "revision": "5002026598ed5d61cd3d68aaaa6f8e02",
+    "url": "/static/js/2.9438baa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a35910a300bef4c4a106",
+    "url": "/static/js/main.84421c48.chunk.js"
+  },
+  {
+    "revision": "11cd5746fb0c968a9319",
+    "url": "/static/js/runtime-main.9e29ec40.js"
+  },
+  {
+    "revision": "36b5bab58a18b9c924861a4ccbf1a790",
+    "url": "/static/media/Roboto-Bold.36b5bab5.ttf"
+  },
+  {
+    "revision": "d5c46b134c17b2138dcd1bb0efa67049",
+    "url": "/static/media/Roboto-Light.d5c46b13.ttf"
+  },
+  {
+    "revision": "9745415b8a2a8b9a2480a12f7e2d3ad3",
+    "url": "/static/media/Roboto-Medium.9745415b.ttf"
+  },
+  {
+    "revision": "5673da52c98bb6cb33ada5aaf649703e",
+    "url": "/static/media/Roboto-Regular.5673da52.ttf"
+  },
+  {
+    "revision": "d99a4e95e8c0a530ff25cc16e6b0aa43",
+    "url": "/static/media/fondo-login.d99a4e95.png"
+  },
+  {
+    "revision": "2f8fcfd62f621bdadf5075786356cc50",
+    "url": "/static/media/logo.2f8fcfd6.svg"
+  }
+]);
